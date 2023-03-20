cc02fb34d66d2b3fd6e6328b569dc870f94835f3
Phuong Nguyen <phuongsover2@gmail.com>
2023-03-20 18:02:46 +0700

    format App.js

--- src/App.js
+++ src/App.js
@@ -4,32 +4,32 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import { privateRoutes, publicRoutes } from '~/routes/routes';
 import DefaultLayout from '~/layouts/DefaultLayout';
 function App() {
-    return (
-        <>
-            <Routes>
-                {publicRoutes.map((route, i) => {
-                    let Layout = DefaultLayout;
-                    if (route.Layout) {
-                        Layout = route.Layout;
-                    } else if (route.layout === null) {
-                        Layout = Fragment;
-                    }
-                    const Page = route.component;
-                    return (
-                        <Route
-                            path={route.path}
-                            element={
-                                <Layout>
-                                    <Page />
-                                </Layout>
-                            }
-                            key={i}
-                        />
-                    );
-                })}
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        {publicRoutes.map((route, i) => {
+          let Layout = DefaultLayout;
+          if (route.Layout) {
+            Layout = route.Layout;
+          } else if (route.layout === null) {
+            Layout = Fragment;
+          }
+          const Page = route.component;
+          return (
+            <Route
+              path={route.path}
+              element={
+                <Layout>
+                  <Page />
+                </Layout>
+              }
+              key={i}
+            />
+          );
+        })}
+      </Routes>
+    </>
+  );
 }
 
 export default App;
